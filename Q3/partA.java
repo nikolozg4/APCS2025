5328a09777b9f68ee91807dbafed1216b492cc62
nikolozg4 <nikolozg4@nycstudents.net>
2025-06-05 15:29:44 -0400

    Update partA.java

--- Q3/partA.java
+++ Q3/partA.java
@@ -1,1 +1,8 @@
-
+public Round(String[] names)
+  {
+  competitorList = new Arraylist<Competitor>();
+  for (int i =0; i < names.length; i++)
+    {
+      competitorList.add(new Competitor(names[i], i+1));
+    }
+}
