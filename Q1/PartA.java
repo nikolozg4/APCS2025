15c800795c379240212e3446993659c639a1892a
nikolozg4 <nikolozg4@nycstudents.net>
2025-06-05 15:11:03 -0400

    PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,1 +1,14 @@
-
+public int walkDogs(int hour) 
+{
+  int availableDogs = company.numAvailableDogs(hour)
+  int dogsWalked=0
+  if (availableDogs <= maxDogs) 
+  {
+    dogsWalked = availableDogs;
+  }
+  else
+  {
+    dogsWalked = maxDogs;
+  }
+  company.updateDogs(hour, dogsWalked);
+  return dogsWalked;
