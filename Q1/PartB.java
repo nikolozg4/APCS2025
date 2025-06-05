e8a676dd28ff355dae0d65cc0640f2f82b633559
nikolozg4 <nikolozg4@nycstudents.net>
2025-06-05 15:16:02 -0400

    Update PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,1 +1,16 @@
-
+public int dogWalkShift(int startHour, int endHour)
+  {
+    int dogsWalked = 0;
+    int extra = 0;
+    for (int i = startHour; i<endHour; i++)
+    {
+        int alreadyWalked = walkDogs(i);
+        dogsWalked += alreadyWalked;
+        if (i>=9 && i<=17 || alreadyWalked == maxDogs)
+        {
+          extra++;
+        }
+    }
+    return dogsWalked+3*extra;
+    
+  }
