5bcb835641c7d5c97addfa69aff92f5afde81678
nikolozg4 <nikolozg4@nycstudents.net>
2025-06-05 15:25:10 -0400

    Update Main.java

--- Q2/Main.java
+++ Q2/Main.java
@@ -1,1 +1,35 @@
+public class SignedText
+  {
+    private String name;
+    private String last;
 
+    public SignedText(String name, String last)
+    {
+      this.name=name;
+      this.last=last;
+    }
+    public String getSignature()
+    {
+      if(name.length()==0)
+      {
+        return last;
+      }
+      return name.substring(0,1)+" "+last;
+    }
+    public String addSignature(String text)
+    {
+      String sign=getSignature();
+      if(text.length() >= sign.length())
+      {
+        if(text.substring(0,sign.length()).equals(sign))
+        {
+          return text.substring(sign.length()) + sign;
+        }
+        else if (text.substring(inputText.length()-sign.length()).equals(sign)
+                 {
+          return text;
+        }
+      }
+      return text+sign;
+    }
+  }
