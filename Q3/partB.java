e8e207ec811ad76554720adb7ec20c551f3a9fd8
nikolozg4 <nikolozg4@nycstudents.net>
2025-06-05 15:36:54 -0400

    Update partB.java

--- Q3/partB.java
+++ Q3/partB.java
@@ -1,1 +1,15 @@
-
+public ArrayList<Match> matches = new ArrayList<match>();
+{
+  ArrayList<Match> matches = nmew ArrayList<Match>();
+  int len = competitorList.size();
+  int begin = 0;
+  if(len%2==1)
+  {
+    begin = 1;
+  }
+  for (int i = 0; in <(len-begin)/2; i++)
+    {
+      matches.add(enw Match(competitorList.get(begin+i), competitorList.get(size -1 -i)));
+    }
+  return matches;
+}
